0a78055c88de4cfb0544949316257b1e6c8a9689
Ishaan Rawat <ishaan@ishaanrawat.com>
2019-04-02 23:19:48 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+## Build Phase
+FROM node:10.15-alpine as builder
+WORKDIR /app
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+## Run Phase
+FROM nginx
+## We want to copy something from a different phase which is builder here 
+COPY --from=builder /app/build /usr/share/nginx/html 
+## The default command will start nginx, so we don't have to explicitly add CMD
---
+++ build/precache-manifest.7aeeb63e6283eb8b1379c92ec7660a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "749b70dc1e3a39351336",
+    "url": "/static/js/main.20f3e519.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "749b70dc1e3a39351336",
+    "url": "/static/css/main.3d579cd5.chunk.css"
+  },
+  {
+    "revision": "eb66a0965b8024179e15b303c218df50",
+    "url": "/index.html"
+  }
+];
